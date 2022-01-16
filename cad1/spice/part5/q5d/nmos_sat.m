fef327ce54981c9ac398c501921ce53d0893ee1c
Jung-Jin Park <jpark25@klone1.hyak.uw.edu>
2022-01-15 21:32:52 -0800

    cad2 gitignore

--- cad1/spice/part5/q5d/nmos_sat.m
+++
@@ -1,8 +0,0 @@
-function IDS = nmos_sat (x,alpha,Vth,n)
-    %Vgs = x;
-    %n = 1.3480;
-    %Vth = 372e-3;
-    %Fit the NMOS equation to the saturation model
-    IDS = alpha.*(x-Vth).^(n);
-
-end
